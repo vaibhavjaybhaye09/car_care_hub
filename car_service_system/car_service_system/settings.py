102850138ce297b26f08e9945c2538d1d90ed9da
Bhushan Lokare <lokarebhushan1@gmail.com>
2025-11-10 15:19:12 +0530

    changes in templates also add new templates deal with urls and deal with view

--- car_service_system/car_service_system/settings.py
+++ car_service_system/car_service_system/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'car_service_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- car_service_system/garage/forms.py
+++ car_service_system/garage/forms.py
@@ -1,14 +1,15 @@
 from django import forms
-from .models import Garage, GarageService
+from .models import Garage, GarageService , ServiceType
 
 
 class GarageProfileForm(forms.ModelForm):
     class Meta:
         model = Garage
-        fields = ['name', 'phone', 'address', 'city', 'state', 'pincode', 'description']
+        fields = ['name', 'phone', 'address', 'city', 'state', 'pincode', 'description','image']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
             'address': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
             'city': forms.TextInput(attrs={'class': 'form-control'}),
             'state': forms.TextInput(attrs={'class': 'form-control'}),
@@ -26,4 +27,13 @@ class GarageServiceForm(forms.ModelForm):
             'price': forms.NumberInput(attrs={'class': 'form-control'}),
             'duration_minutes': forms.NumberInput(attrs={'class': 'form-control'}),
             'custom_text': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+class ServiceTypeForm(forms.ModelForm):
+    class Meta:
+        model = ServiceType
+        fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter service name'}),
         }
